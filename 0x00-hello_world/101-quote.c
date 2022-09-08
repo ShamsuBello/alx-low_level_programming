1b26c7a2f032f3adf88add5b8324453d981b9366
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-09-08 21:46:22 +0000

    printout

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,13 @@
 /**
  * main - Entry point
  *
- * return: Always 1(sucess)
+ * Description - prints Dora's quote 
+ *
+ * Return: Always 1(sucess)
  */
 
 int main(void)
 {
-	write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
