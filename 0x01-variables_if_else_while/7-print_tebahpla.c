127a4439a4492679b9da45090bc0e8e976ba43b3
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-09-09 23:40:30 +0000

    print alphabet in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in reverse order
+ *
+ * Return: aLways 0(sucesss)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c>= 'a'; c--)
+	{
+		putchar(c)
+	}
+	putchar('\n');
+
+	return (0);
+}
