343834d7c49c7235d72b13ca67c6acb198a5af19
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-09-09 23:22:27 +0000

    print numbers 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 0 to 9
+ *
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(p);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
+
