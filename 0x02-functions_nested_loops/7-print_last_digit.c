56a672ae02f6647fd73a6bbc4afb5287857c509d
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-09-13 21:47:51 +0000

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_last_digit - function that prints the last digit 
+ * print_last_digit - function that prints the last digit
  * @n: takes in an integer
  * Return: return output
  */
