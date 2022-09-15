9417f096bda13673efb232e4886c1bf9ae047045
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-09-15 15:06:46 +0000

    prints numbers from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *print_numbers - function that print all numbers from
- *0 to 9
+ * print_numbers - function that print all numbers from
+ * 0 to 9
  *
- *Return: returns nothing
+ * Return: returns nothing
  */
 void print_numbers(void)
 {
