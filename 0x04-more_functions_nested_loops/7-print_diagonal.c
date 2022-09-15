b424b853f44f3e19145c2c4dd2edc115fd68b893
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-09-15 22:23:14 +0000

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,12 +1,10 @@
-
+#include <stdio.h>
 #include "main.h"
-
 /**
  * print_diagonal - prints a diagonal
  * @n: parameter
  * Return: returns nothing
  */
-
 void print_diagonal(int n)
 {
 	int len, space;
@@ -16,16 +14,12 @@ void print_diagonal(int n)
 		for (len = 0; len < n; len++)
 		{
 			for (space = 0; space < len; space++)
-			{
 				putchar(' ');
-			}
 
 			putchar('\\');
 
 			if (len == (n - 1))
-			{
 				continue;
-			}
 			putchar('\n');
 		}
 	}
