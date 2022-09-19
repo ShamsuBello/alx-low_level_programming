89d08a2bface18cc5c879ca4705ef7cf5906be27
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-09-19 23:01:49 +0000

    print string in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,22 +8,18 @@
 
 void rev_string(char *s)
 {
-	char *t = s;
-	char n[1000];
-	short c = 0;
+	char rev = s[0];
+	int fcounter = 0;
+	int i;
 
-	while (*s != '\0')
-	{
-		n[c] = *s;
-		s++;
-		c++;
-	}
-	c = 0;
+	while (s[fcounter] != '\0')
+		fcounter++;
 
-	while (s > t)
+	for (i = 0; i < fcounter; i++)
 	{
-		s--;
-		*s = n[s];
-		c++;
+		fcounter--;
+		rev = s[i];
+		s[i] = s[fcounter];
+		s[fcounter] = rev;
 	}
 }
