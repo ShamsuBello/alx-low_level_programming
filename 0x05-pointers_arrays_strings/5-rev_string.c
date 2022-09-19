f0ba1b27611b8d331fb819d6fd795a4691e31fc8
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-09-19 22:33:23 +0000

    print a string in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string in reverse
+ * @s: arg s
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i, j;
+	char str[1000];
+
+	i = 0;
+
+	while (*(s + 1) != 0)
+	{
+		str[i] = *(s + 1);
+		i++;
+	}
+
+	j = i - 1;
+	i = 0;
+
+	while (j >= 0)
+	{
+		*(s + j) = str[i];
+		j--;
+		i++;
+	}
+}
