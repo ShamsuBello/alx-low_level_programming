5477c7c86f0817df888d7579bd2dc4c2edc4e582
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-09-22 13:59:07 +0000

    concartinate two numbers and add inputed numbers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,10 +10,18 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int index, count;
 
-	while (dest[index++] && index < n; index ++)
-		dest[dest_len++] = src[index];
-
+	for (index = 0; dest[index] != '\0'; index++)
+		;
+	for (count = 0; count < n && src[count] != '\0'; count++)
+	{
+		dest[index] = src[count];
+		index++;
+	}
+	if (count < n)
+	{
+		dest[index] = '\0';
+	}
 	return (dest);
 }
