d7f0a24f5033f32ea5781c3817eb4ae59e7146b2
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-09-22 14:05:54 +0000

    comapre two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,10 +4,7 @@
  * _strcmp - functions which compare two strings
  * @s1: first string
  * @s2: second string
- * Return: 
- * 		returns zero if s1 == s2
- * 		returns negative number if s1 < s2
- * 		returns positive number if s1 > s2
+ * Return: If the strings are equals return "0", if not return other number
  */
 
 int _strcmp(char *s1, char *s2)
