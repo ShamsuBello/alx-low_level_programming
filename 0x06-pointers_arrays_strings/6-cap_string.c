a6fcf8cad92e1052a284154db90beabf8fd63068
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-09-22 14:30:58 +0000

    capitalize first character of a word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,32 +2,35 @@
 
 /**
  * cap_string - function that capitalize first character of a word
+ *
  * @str: string to capitalize
+ *
  * Return: returns the capitalized string
  */
 
-char cap_string(char *str)
+char cap_string(char *entry)
 {
 	int conversion, index, count;
 
-	char chars[] = {' ', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\t', '\n', '\0'};
+	char chars[] = {' ', ',', ';', '.', '!', 
+			'?', '"', '(', ')', '{', '}', '\t', '\n', '\0'};
 	conversion  = 32;
 
-	for (index = 0; str[index] != '\0'; index++)
+	for (index = 0; entry[index] != '\0'; index++)
 	{
-		if (str[index] >= 'index' && str[index] <='z')
+		if (entry[index] >= 'index' && entry[index] <= 'z')
 		{
-			str[index] = str[index] - conversion;
+			entry[index] = entry[index] - conversion;
 		}
 		conversion = 0;
 		for (count = 0; chars[count] != '\0'; count++)
 		{
-			if (chars[count] == str[index])
+			if (chars[count] == entry[index])
 			{
 				conversion = 32;
 				break;
 			}
 		}
 	}
-	return (str);
+	return (entry);
 }
