bbc55b71a0d70fdae6f4c6a2968798ef67d97a2b
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-09-22 14:32:54 +0000

    function to capitalize a word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * cap_string - function that capitalize first character of a word
+ * cap_string - Write a function that capitalizes all words of a string
  *
- * @str: string to capitalize
+ * @entry: string to capitalize
  *
  * Return: returns the capitalized string
  */
