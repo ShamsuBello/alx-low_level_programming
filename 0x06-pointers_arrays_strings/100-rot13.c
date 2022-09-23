2b8a38df96a4b7c8750bcfe3efdedb0681ec49d1
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-09-23 12:05:19 +0000

    rot13 encoder

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * rot13 - Write a function that encodes a string using rot13
  * @hi: This is my input string
@@ -10,7 +9,6 @@ char *rot13(char *hi)
 	int index, count;
 	char minus[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char mayus[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
 	for (index = 0; hi[index] != '\0'; ++index)
 	{
 		for (count = 0; minus[count] != '\0' ; count++)
