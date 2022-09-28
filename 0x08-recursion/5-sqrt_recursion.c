d30fe5218734b16de41c9387bc3120351d20a57c
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-09-28 09:17:57 +0000

    evaluate function sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,13 +9,14 @@
 
 int _evaluate(int i, int n)
 {
-	/* evaluate function*/
+	/*evaluate function*/
 	if (n == 0 || n == 1)
 		return (n);
+
 	else if (i * i < n)
 		return (_evaluate(i + 1, n));
 
-	else if ( i * i == n) /* Condition base*/
+	else if (i * i == n) /* Condition base*/
 		return (i);
 
 	return (-1);
