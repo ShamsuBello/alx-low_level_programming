753b1b58a73d9f7733a4345814d78e95c7214311
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-09-30 18:14:40 +0000

    print number of argument

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,13 +5,12 @@
  * main - print the name of the program
  * @argc: Count arguments
  * *argv: Arguments
- *
- * Return: Always 0 (sucess)
+ * Return: Always 0(sucess)
  */
 
 int main(int argc, char *argv[])
 {
-	(void) argv; /*Ignore argv*/
+	(void)argv; /*Ignore argv*/
 	printf("%i\n", argc - 1);
 
 	return (0);
