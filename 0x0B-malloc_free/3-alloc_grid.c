6a1d38fff3323cd04379fbd53d32a73e0ef8faaf
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-10-03 15:29:04 +0000

    function to retturn pointer in 2 dimensional

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * **alloc_grid - functions that returns a pointer to a 2 dimensional array of integers
+ * @width: Column of the array
+ * @height: rows of the array
+ *
+ * Return: A pointer to a 2 dimensional array of integers
+ */
+
+int **alloc_grid(int width, int height) /* 6 columns and 4 rows*/
+{
+	int j, i;
+	int **arr;
+	
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	arr = (int **)malloc(sizeof(int *) * height);
+	if (arr == NULL)
+	{
+		free(arr);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			for (i = 0; i < height; i++)
+			{
+				free(arr);
+				return (NULL);
+			}
+		}
+
+		for (i = 0; i < height; i++)
+			for (j = 0; j < width; j++)
+			{
+				arr[i][j] = 0;
+			}
+
+		return (arr);
+	}
+}
