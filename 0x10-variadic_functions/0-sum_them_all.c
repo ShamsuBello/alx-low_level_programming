c58193f1da6e09838f4dfed78e3659bba40948ec
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-10-13 18:59:28 +0000

    sums ofa ll parameter

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,4 @@
 #include <stdarg.h>
-#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
@@ -11,17 +10,17 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int count;
-	int sum;
+	int sum = 0;
+	unsigned int i;
 
-	sum = 0;
 	va_start(ap, n);
 
 	if (n != 0)
 	{
-	for (count = 0; count < n; count++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
 	}
+
 	va_end(ap);
 	return (sum);
 }
