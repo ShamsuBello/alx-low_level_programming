fb223d790025bdced01320dc076a9c6652a794a0
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-10-13 22:01:47 +0000

    prints all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,11 +1,12 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
  * print_all - prints all
  * @format: param
  */
-
-void print_all(const char * const format, ...)
+void print_all(const char *const format, ...)
 {
 	va_list valist;
 	unsigned int i = 0, j, k = 0;
