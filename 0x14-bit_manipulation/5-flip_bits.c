9d259f93e731096b2b1ba1ea6e60c1d59670f9c0
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-10-27 22:07:25 +0000

    counts the number of bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * flip_bits - counts the number of bits needed to be flipped
+ * flip_bits - counts the number of bits needed to be flipped.
+ * to get from one number to another
  * @n: the number
  * @m: the number to flip n to
+ *
  * Return: The necessary number of bits to flip to get from n to m
  */
 
