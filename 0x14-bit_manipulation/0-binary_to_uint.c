c2da90177c3a1479c0d4804e9ad030500a77e778
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-10-27 20:16:56 +0000

    convert binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 /**
  * binary_to_uint - converts a binary to an unsigned int
  * @b: pointer to a string containing a binary number
- * Return: unsigned int with decimal value of binary number
+ * Return: unsigned int with decimal value of binary number, or 0 if error
  */
 
 unsigned int binary_to_uint(const char *b)
