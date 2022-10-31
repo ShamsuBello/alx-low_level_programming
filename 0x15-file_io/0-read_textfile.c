306fc10091f850553fb676f859709513ffc1bf53
Shamsudeen Bello <Shamsubello007@gmail.com>
2022-10-31 19:26:02 +0000

    read text file and print

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout
- * @filename: A pointer to the name of the file
- * @letters: The number of letters, the function should read and print
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print
  *
  * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print
+ *         o/w - the actual number of bytes the function can read and print
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -25,6 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
+
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
